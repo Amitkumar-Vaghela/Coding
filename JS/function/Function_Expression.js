c91b4876f8eb2bd037518bb5ed56523da76b50cd
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-09-07 12:19:54 +0530

    Update Function_Expression.js

--- JS/function/Function_Expression.js
+++ JS/function/Function_Expression.js
@@ -1,8 +1,4 @@
 // Function Expression
-// A function expression is when a function is assigned to a variable.
-// Unlike function declarations, function expressions are not hoisted,
-// so they cannot be called before they are defined.
-
 const greet = function() {
     console.log("A function expression is when a function is assigned to a variable. Unlike function declarations, function expressions are not hoisted, so they cannot be called before they are defined.");
 };
